61ca89aad7f825fab234da277729d0e8f386e944
TBWanderer <evgenypodyryaka@yandex.ru>
2024-07-21 00:42:33 +0300

    Fix laptop configuration

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
 
 	outputs = { nixpkgs, home-manager, ... }@inputs: {
 		nixosConfigurations = {
-			laptop = {
+			laptop = nixpkgs.lib.nixosSystem {
 				system = "x86_64-linux";
 				specialArgs = { inherit inputs; };
 				modules = [
