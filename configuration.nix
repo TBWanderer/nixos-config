ce67d1e7551a72055374b59e6d651cdc233170df
TBWanderer <evgenypodyryaka@yandex.ru>
2024-07-20 17:32:10 +0300

    Fixed some things

--- configuration.nix
+++ configuration.nix
@@ -5,125 +5,195 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+	imports =
+		[ # Include the results of the hardware scan.
+			./hardware-configuration.nix
+	];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+	# BOOT-part
+	boot = {
+		loader = {
+			systemd-boot = {
+				enable = false;
+			};
+			efi = {
+				canTouchEfiVariables = false;
+			};
+			grub = {
+				enable = true;
+				efiInstallAsRemovable = true;
+				efiSupport = true;
+				device = "nodev";
+			};
+		};
+		supportedFilesystems = [
+			"ntfs"
+		];
+	};
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+	# networking.hostName = "nixos"; # Define your hostname.
+	# Pick only one of the below networking options.
+	# networking.wireless.enable = true;	# Enables wireless support via wpa_supplicant.
+	networking.networkmanager.enable = true;	# Easiest to use and most distros use this by default.
 
-  # Set your time zone.
-  time.timeZone = "Europe/Moscow";
+	# Set your time zone.
+	time.timeZone = "Europe/Moscow";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+	# Configure network proxy if necessary
+	# networking.proxy.default = "http://user:password@host.com:1234/";
+	# networking.proxy.noProxy = "127.0.0.1,localhost";
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
+	# Select internationalisation properties.
+	# i18n.defaultLocale = "en_US.UTF-8";
+	# console = {
+	#	 font = "Lat2-Terminus16";
+	#	 keyMap = "us";
+	#	 useXkbConfig = true; # use xkb.options in tty.
+	# };
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
+	# Enable the X11 windowing system.
+	# services.xserver.enable = true;
 
+	
+	hardware = {
+		bluetooth = {
+			enable = true;
+			powerOnBoot = true;
+		};
+	};
 
-  
+	# Configure keymap in X11
+	# services.xserver.xkb.layout = "us";
+	# services.xserver.xkb.options = "eurosign:e,caps:escape";
 
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+	# Enable CUPS to print documents.
+	# services.printing.enable = true;
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+	# Enable sound.
+	# hardware.pulseaudio.enable = true;
+	# OR
+	services = {
+		pipewire = {
+			enable = true;
+			pulse.enable = true;
+		};
+		blueman = {
+			enable = true;
+		};
+	};
 
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
+	# Enable touchpad support (enabled default in most desktopManager).
+	# services.libinput.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
+	users.users.x = {
+		isNormalUser = true;
+		extraGroups = [ "wheel" ];
+	#	 packages = with pkgs; [
+	#		 firefox
+	#		 tree
+	#	 ];
+	};
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     tree
-  #   ];
-  # };
+	# List packages installed in system profile. To search, run:
+	# $ nix search wget
+	environment.systemPackages = with pkgs; [
+		# ( import ./lockscreen.nix )
+		vim 
+		wget
+		neovim
+		gitFull
+		github-cli
+		lynx
+		hyprland
+		kitty
+		alacritty
+		fish
+		brave
+		unzip
+		gcc
+		waybar
+		rofi-wayland
+		audacity
+		bluez
+		blueman
+		cinnamon.nemo
+		zulu17
+		exiftool
+		swayimg
+		cliphist
+		swaylock-effects
+		grim
+		slurp
+		wl-clipboard
+		lazygit
+	];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    neovim
-    gitFull
-    github-cli
-  ];
+	fonts.packages = with pkgs; [
+		(nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+	];
 
-  programs.git = {
-    enable = true;
-  };
+	programs = {
+		git = {
+			enable = true;
+		};
+		hyprland = {
+			enable = true;
+		};
+		fish = {
+			enable = true;
+		};
+	};
+	
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+	nix = {
+	package = pkgs.nixFlakes;
+	extraOptions = ''
+		experimental-features = nix-command flakes
+	'';
+	};
 
-  # List services that you want to enable:
+	# Some programs need SUID wrappers, can be configured further or are
+	# started in user sessions.
+	# programs.mtr.enable = true;
+	# programs.gnupg.agent = {
+	#	 enable = true;
+	#	 enableSSHSupport = true;
+	# };
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+	# List services that you want to enable:
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+	# Enable the OpenSSH daemon.
+	services.openssh.enable = true;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+	# Open ports in the firewall.
+	# networking.firewall.allowedTCPPorts = [ ... ];
+	# networking.firewall.allowedUDPPorts = [ ... ];
+	# Or disable the firewall altogether.
+	# networking.firewall.enable = false;
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.05"; # Did you read the comment?
+	# Copy the NixOS configuration file and link it from the resulting system
+	# (/run/current-system/configuration.nix). This is useful in case you
+	# accidentally delete configuration.nix.
+	# system.copySystemConfiguration = true;
+
+	# This option defines the first version of NixOS you have installed on this particular machine,
+	# and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+	#
+	# Most users should NEVER change this value after the initial install, for any reason,
+	# even if you've upgraded your system to a new NixOS release.
+	#
+	# This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+	# so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+	# to actually do that.
+	#
+	# This value being lower than the current NixOS release does NOT mean your system is
+	# out of date, out of support, or vulnerable.
+	#
+	# Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+	# and migrated your data accordingly.
+	#
+	# For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+	system.stateVersion = "24.05"; # Did you read the comment?
 
 }
 
