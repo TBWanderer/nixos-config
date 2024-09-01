7fc97efcc56a90a1366c9536e6ebf30518433dec
TBWanderer <evgenypodyryaka@yandex.ru>
2024-09-01 17:54:36 +0300

    Fixed cava

--- home/shell/cava/default.nix
+++ home/shell/cava/default.nix
@@ -1,38 +1,38 @@
-{pkgs, ...}:
+{ pkgs, ... }:
 pkgs.writeShellScriptBin "cava_waybar" ''
-	set -e
+   set -e
 
-	bar="▁▂▃▄▅▆▇█"
-	dict="s/;//g;"
-	i=0
-	while [ $i -lt ''${#bar} ]
-	do
-		dict="''${dict}s/$i/''${bar:$i:1}/g;"
-		i=$((i=i+1))
-	done
+   bar="▁▂▃▄▅▆▇█"
+   dict="s/;//g;"
+   i=0
+   while [ $i -lt ''${#bar} ]
+   do
+      dict="''${dict}s/$i/''${bar:$i:1}/g;"
+      i=$((i=i+1))
+   done
 
-	pipe="/tmp/cava.fifo"
-	if [ -p $pipe ]; then
-		unlink $pipe
-	fi
-	mkfifo $pipe
+   pipe="/tmp/cava.fifo"
+   if [ -p $pipe ]; then
+      unlink $pipe
+   fi
+   mkfifo $pipe
 
-	config_file="/tmp/polybar_cava_config"
-	echo "
-	[general]
-	bars = 20
-	framerate = 60
+   config_file="/tmp/polybar_cava_config"
+   echo "
+   [general]
+   bars = 20
+   framerate = 60
 
-	[output]
-	method = raw
-	raw_target = $pipe
-	data_format = ascii
-	ascii_max_range = 7
-	" > $config_file
+   [output]
+   method = raw
+   raw_target = $pipe
+   data_format = ascii
+   ascii_max_range = 7
+   " > $config_file
 
-	${pkgs.cava}/bin/cava -p $config_file &
+   ${pkgs.cava}/bin/cava -p $config_file &
 
-	while read -r cmd; do
-		echo $cmd | sed $dict
-	done < $pipe
+   while read -r cmd; do
+      echo $cmd | sed $dict
+   done < $pipe
 ''
