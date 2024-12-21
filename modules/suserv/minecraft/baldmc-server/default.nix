6a53209a734156dc932d059347e4af38edf54a56
TBWanderer <evgenypodyryaka@yandex.ru>
2024-12-21 19:41:20 +0300

    MineServChanges

--- modules/suserv/minecraft/baldmc-server/default.nix
+++ modules/suserv/minecraft/baldmc-server/default.nix
@@ -8,11 +8,11 @@
     config = let cfg = config.server.minecraft.baldmc-server; in lib.mkIf cfg.enable {
         flux = {
             servers = {
-                main = {
+                baldmc-server = {
                     package = pkgs.mkMinecraftServer {
                         name = "baldmc-server";
                         src = ./config;
-                        hash = "sha256-GiNNrOLZsPFVGLRnkeqanCOESlTQSDyfWZijZjuLVz4=";
+                        hash = "sha256-C5Li6PctW2lPsGhsLQn8YlGKJ+VL3kq/fr3vMR6pxpg=";
                     };
                     proxy.enable = false;
                 };
